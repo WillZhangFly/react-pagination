a321869897c62181ae7882ef895d12d397abd604
WillZhangTR <w.zhang@thomsonreuters.com>
2021-08-30 23:17:39 -0400

    init commit for pagination

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Posts } from './components/Posts';
+import { Pagination } from './components/Pagination';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  //change page
+  const paginate = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  };
+
+  return (
+    <div className="Container mt-5">
+      <h1 className="text-primary mb-3">My Posts</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+}
+
+export default App;
